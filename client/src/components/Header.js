966ebaa7397d5c8dcbc3441c36a2571428f45691
developerjhp <developerjhp@gmail.com>
2021-10-27 00:00:04 +0900

    add : Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,45 @@
 import { Link } from 'react-router-dom'
+// import { Nav, , NavDropdown, Container, Navbar } from 'react-bootstrap'
+// import { LinkContainer } from 'react-router-bootstrap';
+import { useState } from 'react'
+import styled from 'styled-components'
+
+const StyledHeader = styled.header`
+  padding: 1rem 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: start;
+
+  li {
+    padding: 2px 0px;
+    text-align: center;
+  }
+`
 
 export default function Header () {
-  return <header>
-    <Link to='/main'>Minimanimo</Link>
+  const [show, setShow] = useState(false);
+  const showDropdown = () => {
+    setShow(!show);
+  }
+  const hideDropdown = () => {
+    setShow(false);
+  }
+  // 프로필 이미지, 닉네임
+  // hover => drop down 기능 구현
+  return <StyledHeader className="header">
     <div>
-      {/* 프로필 이미지, 닉네임 */}
-      {/* hover => drop down 기능 구현 */}
+      <Link to='/' className="logo">Minimanimo</Link>
     </div>
-    <ul className="nav">
+    <div className="profile" onMouseLeave={hideDropdown} show={show}
+      onMouseEnter={showDropdown}>
+      <div>hover 영역
+    {show ? 
+    <ul>
       <li><Link to="/mypage">Mypage</Link></li>
       <li>Logout</li>
     </ul>
-  </header>
-}
+    : null }
+    </div>
+    </div>
+  </StyledHeader>
+};
--- client/src/page/Mypage.js
+++ client/src/page/Mypage.js
@@ -1,5 +1,9 @@
+import { Link } from 'react-router-dom'
+import Header from '../components/Header';
+
 const Mypage = () => {
   return <>
+    <Header />
   </>
 }
 
