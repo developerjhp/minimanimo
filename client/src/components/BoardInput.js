595eaeab3be1dadf2ff17105468e4333c6cf5972
developerjhp <developerjhp@gmail.com>
2021-10-30 20:07:55 +0900

    feat : Board , BoardList #1

--- client/src/components/BoardInput.js
+++ client/src/components/BoardInput.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
-import Symbol from '../assets/images/Symbol.svg';
 import { Link } from 'react-router-dom'
 
 const NewTweetWrap = styled.div`
@@ -24,10 +23,12 @@ const NewTweetWrap = styled.div`
   }
 
   input {
-    line-height: 10vh;
+    height: 10vh;
+    line-height: 1;
     border: 1px solid #e6e6e6;
     flex: 7 0 0;
   }
+  
   input:focus {
     outline: 1px solid #EDC51E;
   }
@@ -50,6 +51,13 @@ const SubmitBtn = styled.button`
   color: #fff;
   background: #387099;
   flex: 2 0 0;
+
+  &:hover {
+    background: #EDC51E;
+  }
+  &:disabled {
+    background: #aaa;
+  }
 `
 
 export default function BoardInput({ isLogedIn }) {
@@ -64,7 +72,7 @@ export default function BoardInput({ isLogedIn }) {
 
   const config = {
     headers: {
-      'Authorization': `Bearer ${newInputInfo.token}`,
+      Authorization: isLogedIn ? `Bearer ${newInputInfo.token}` : undefined,
       'Content-Type': 'application/json',
     },
   };
@@ -72,17 +80,20 @@ export default function BoardInput({ isLogedIn }) {
   const submitBtnHandler = () => {
     //axios post 요청 (_id(localstorage userInfo._id)), 토큰(header), newInputValue 전송)
     //서버에서 외부에서 못들어오도록 막아서 이거 할때 토큰도 같이 보내야 함.
-    axios.post('/api/posts/new', { content: newInputValue, user: newInputInfo }, config)
-      .then(res => {
-        console.log("잘되었어요")
-      }).catch(err => {
-        console.log(err)
-      })
+    if (newInputValue !== '') {
+      axios.post('/api/posts/new', { content: newInputValue }, config)
+        .then(res => {
+          console.log("잘되었어요")
+          setNewInputValue('')
+        }).catch(err => {
+          console.log(err)
+        })
+    }
   }
 
   return (
     <NewTweetWrap>
-      <div><img src={Symbol} alt="프로필 이미지" /></div>
+      <div><img src={isLogedIn ? JSON.parse(localStorage.getItem('userInfo')).image : '/images/users/1.jpeg'} alt="프로필 이미지" /></div>
       <Nickname >{isLogedIn ? JSON.parse(localStorage.getItem('userInfo')).nickname : <Link to='/signin'>로그인을 해주세요</Link>}</Nickname>
       {isLogedIn
         ? <input type="text" value={newInputValue} onChange={newInputValueHandler} maxLength={255} />
--- client/src/components/BoardList.js
+++ client/src/components/BoardList.js
@@ -1,9 +1,43 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 import Board from './Board';
 
+const BoardListWrap = styled.div`
+  height: 60vh;
+  margin: 2rem 2rem 0;
+  padding: 1.5rem;
+  text-align: left;
+  align-items: center;
+  background: #fff;
+  border-radius: 0.4rem;
+  box-shadow: 0 0 0.4rem 0.2rem #eee;
+  display: flex;
+  flex-direction : row;
+
+`
+
 export default function BoardList({ isLogedIn }) {
-  return <>
-  </>
+
+  const [boardListData , setBoardListData] = useState('')
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  useEffect(() => {
+    axios.get('/api/posts', config)
+    .then(res=> {
+      //res.data에 들어가 있음, 
+      setBoardListData(res.data)
+      
+    }).catch(err=> {console.log(err)})
+  }, [])
+
+
+  return <BoardListWrap>
+    <Board boardListData={boardListData}/>
+  </BoardListWrap>
 };
