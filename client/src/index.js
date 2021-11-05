cc0b8abba9cee7d1e11c58535b46233984305ca1
developerjhp <developerjhp@gmail.com>
2021-11-06 01:47:19 +0900

    fix : 이스터에그 고양이 color 색 변경  (라이트모드에서 안보여서변경함)

--- client/src/index.js
+++ client/src/index.js
@@ -29,7 +29,7 @@ ReactDOM.render(
                     \\__                             __/\n\
                         \\_____                _____/\n\
                               \\______________/\n\
-          \n", 'color:#ff6905','color:white','color:#ff6905','color:white','color:#ff6905','color:white','color:#ff6905','color:white','color:#ff6905','color:white')}
+          \n", 'color:#ff6905','color:defalut','color:#ff6905','color:defalut','color:#ff6905','color:defalut','color:#ff6905','color:defalut','color:#ff6905','color:defalut')}
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -39,30 +39,30 @@ ReactDOM.render(
 // ReactDOM.render(
 //   <React.StrictMode>
 //     <App />
-//     {console.log("                                                                        ,,\n\
-//                                                                        /  ,\n\
-//                                                                       /   /\n\
-//                                                                      /   /\n\
-//                                                                     /   /\n\
-//     __________________________                                     /   /\n\
-//     ⎢                         ⎥                                   /   /\n\
-//     ⎢  혹시 내가 보여? 난 고양이야  ⎥                                  /   /\n\
-//     ⎢____    _________________⎥                                 /   /\n\
-//           \\/    ,      ,,                                      /   /\n\
-//                /@\\____/@ \\                                ____/   /\n\
-//               /           \\                         _____/        /__\n\
-//         /\" \\ / •    •      \\                     __/             /  @@\"\\\n\
-//         \\    @@  ㅅ  @@     /___             ___/                /    _/\n\
-//       /\" \\   \\                 \\__________/                    |__/ \"\\\n\
-//       \\   \\                                                   /      /\n\
-//        \\    \\  __                                                  _/\n\
-//         \\                                                       __/\n\
-//           \\_                                             ______/\n\
-//              \\ ___                                     _/\n\
-//                     \\__                             __/\n\
-//                         \\_____                _____/\n\
-//                               \\______________/\n\
-//           \n")}
+    // {console.log("                                                                        ,,\n\
+    //                                                                    /  ,\n\
+    //                                                                   /   /\n\
+    //                                                                  /   /\n\
+    //                                                                 /   /\n\
+    // __________________________                                     /   /\n\
+    // ⎢                         ⎥                                   /   /\n\
+    // ⎢  혹시 내가 보여? 난 고양이야  ⎥                                  /   /\n\
+    // ⎢____    _________________⎥                                 /   /\n\
+    //       \\/    ,      ,,                                      /   /\n\
+    //            /%c@%c\\____/%c@%c \\                                ____/   /\n\
+    //           /           \\                         _____/        /__\n\
+    //     /\" \\ / •    •      \\                     __/             /  %c@@%c\"\\\n\
+    //     \\    %c@@%c  ㅅ  %c@@%c     /___             ___/                /    _/\n\
+    //   /\" \\   \\                 \\__________/                    |__/ \"\\\n\
+    //   \\   \\                                                   /      /\n\
+    //    \\    \\  __                                                  _/\n\
+    //     \\                                                       __/\n\
+    //       \\_                                             ______/\n\
+    //          \\ ___                                     _/\n\
+    //                 \\__                             __/\n\
+    //                     \\_____                _____/\n\
+    //                           \\______________/\n\
+    //       \n", 'color:#ff6905','color:defalut','color:#ff6905','color:defalut','color:#ff6905','color:defalut','color:#ff6905','color:defalut','color:#ff6905','color:defalut')}
 //   </React.StrictMode>,
 //   document.getElementById('root')
 // );
